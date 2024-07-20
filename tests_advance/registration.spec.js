a2636cb7c6b2026f29713bfa233639d9e8dda456
Elgrueber <elenagrueber@gmail.com>
2024-07-20 19:25:07 -0400

    add tab

--- tests_advance/registration.spec.js
+++ tests_advance/registration.spec.js
@@ -1,24 +1,10 @@
 import { expect } from '@playwright/test';
 import { test } from '../fixtures/base';
-import { retrieveUserEmailConfirmationLink, retrieveUserEmailConfirmCode } from '../helpers/utils';
 import {
     URL_END_POINTS,
-    BUSINESS_MONTHLY_PLAN,
-    CARD_DETAILS,
-    FREE_PLAN_DESCRIPTION,
-    SUBSCRIPTIONS,
-    SUBSCRIBE_TO_PERSONAL_PLAN,
-    SUBSCRIBE_TO_BUSINESS_PLAN,
-    PLEASE_ENTER_CONFIRMATION_CODE,
-    PERSONAL_PLAN_DESCRIPTION,
-    BUSINESS_PLAN_DESCRIPTION,
-    NO_ATTACHED_CARD,
-    EMAIL_SUBJECTS,
-    QASE_LINK,
-    GOOGLE_DOC_LINK,
 } from '../testData';
 import { generateNewUserData } from '../helpers/utils';
-import { description, tag, severity, Severity, link, epic, step } from 'allure-js-commons';
+import { description, tag, severity, Severity, epic, step } from 'allure-js-commons';
 
 test.describe('Negative tests for Free user Registration', () => {
     test('SP11/SP2/1 | Verify non-successful registration of Free user in case of empty name field', async ({
@@ -36,12 +22,19 @@ test.describe('Negative tests for Free user Registration', () => {
             await page.goto(URL_END_POINTS.signUpFree);
         });
         await signUpFreePage.yourInformation.fillNameInputField("");
+        
+        expect (await signUpFreePage.yourInformation.nameError).toHaveText('Required');
         await signUpFreePage.yourInformation.fillEmailInputField(newUserData.email);
         await signUpFreePage.yourInformation.fillPasswordInputField(newUserData.password);
 
+        await step('Verify the error message', async () => {
         expect (await signUpFreePage.yourInformation.nameError).toHaveText('Required');
+        });        
 
+        await step('Verify the Create account button is disabled', async () => {       
         const buttonDisabled = await signUpFreePage.createAccountBtn.isDisabled();
         expect (buttonDisabled).toBeTruthy();
+        expect (await signUpFreePage.yourInformation.nameError).toHaveText('Required');
+        }); 
     })
 })
