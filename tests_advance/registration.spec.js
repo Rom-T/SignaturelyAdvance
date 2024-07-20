910d5a796ba606dfcdd779fe217af7bc8049099f
Elgrueber <elenagrueber@gmail.com>
2024-07-20 18:06:51 -0400

    add import

--- tests_advance/registration.spec.js
+++ tests_advance/registration.spec.js
@@ -1,19 +1,24 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import { expect } from '@playwright/test';
+import { test } from '../fixtures/base';
+import { retrieveUserEmailConfirmationLink, retrieveUserEmailConfirmCode } from '../helpers/utils';
+import {
+    URL_END_POINTS,
+    BUSINESS_MONTHLY_PLAN,
+    CARD_DETAILS,
+    FREE_PLAN_DESCRIPTION,
+    SUBSCRIPTIONS,
+    SUBSCRIBE_TO_PERSONAL_PLAN,
+    SUBSCRIBE_TO_BUSINESS_PLAN,
+    PLEASE_ENTER_CONFIRMATION_CODE,
+    PERSONAL_PLAN_DESCRIPTION,
+    BUSINESS_PLAN_DESCRIPTION,
+    NO_ATTACHED_CARD,
+    EMAIL_SUBJECTS,
+    QASE_LINK,
+    GOOGLE_DOC_LINK,
+} from '../testData';
+import { generateNewUserData } from '../helpers/utils';
+import { description, tag, severity, Severity, link, epic, step } from 'allure-js-commons';
 
 test.describe('Negative tests for Free user Registration', () => {
     test('SP11/SP2/1 | Verify non-successful registration of Free user in case of empty name field', async ({
