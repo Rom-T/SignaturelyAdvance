f9ed94070a8a97db176613ba71e16c75494db5ce
Rom-T <Rom-T@users.noreply.github.com>
2024-07-21 19:18:04 +0000

    deploy: 89640a48fdc9f1887490e1c9a3950ee85996c5b5

--- new_pom/pages/templates/apiTemplatesPage.js
+++
@@ -1,8 +0,0 @@
-import TableComponent from '../../components/tableComponent';
-
-export default class ApiTemplatesPage {
-    constructor(page) {
-        this.page = page;
-        this.table = new TableComponent(this.page);
-    }
-}
