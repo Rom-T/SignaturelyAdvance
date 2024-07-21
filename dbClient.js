034bb05bd99f871a0636c4b571dde868ade8b27a
IrynaKolh <IrynaKolh@users.noreply.github.com>
2024-07-21 02:03:17 +0000

    deploy: d729389eeb7162ee7793c38f52427b7307d7aa47

--- dbClient.js
+++
@@ -1,9 +0,0 @@
-import { Client } from 'pg';
-import { parse } from 'pg-connection-string';
-
-export function createNewClient() {
-    const connectionString = process.env.DB_DATA;
-    const config = parse(connectionString);
-
-    return new Client(config);
-}
