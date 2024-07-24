9858fc906f9cfc79cc075fc350540d94e5997f96
Iryna Kolhanova <96072009+IrynaKolh@users.noreply.github.com>
2024-07-24 13:44:42 -0400

    Ik/fix linter (#10)
    * fix linter unused vars

--- tests_advance/settingsCompany.spec.js
+++ tests_advance/settingsCompany.spec.js
@@ -1,8 +1,7 @@
 import { expect } from '@playwright/test';
 import { test } from '../fixtures/base';
-import { NEGATIVE_COMPANY_INFO_ARR, NEGATIVE_FIELD_DATA, TOAST_MESSAGE, URL_END_POINTS } from '../testData';
-import { generateNewUserData } from '../helpers/utils';
-import { description, tag, tags, severity, Severity, epic, feature, step } from 'allure-js-commons';
+import { NEGATIVE_FIELD_DATA, TOAST_MESSAGE } from '../testData';
+import { description, tags, severity, Severity, epic, feature, step } from 'allure-js-commons';
 
 test.describe('Negative tests for Free user Registration', () => {
     test(`SP26/SP30/1 | Verify the creation of a company profile fails when the entered Company Name exceeds 99 characters.`, async ({
@@ -17,8 +16,6 @@ test.describe('Negative tests for Free user Registration', () => {
         await epic('Settings');
         await feature('Company');
         await tags('Company form', 'Negative');
-
-        const newUserData = await generateNewUserData();
 
         await signPage.sideMenu.clickSettings();
         await settingsCompanyPage.fillCompanyName(NEGATIVE_FIELD_DATA);
