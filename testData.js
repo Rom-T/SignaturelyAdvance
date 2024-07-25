992e3ec57028f797cdabecd61951af739aeccb12
alfiyaGumar <alfiia.gumar@gmail.com>
2024-07-24 20:13:15 -0400

    SP-13/SP-7/01Authorization

--- testData.js
+++ testData.js
@@ -281,3 +281,5 @@ export const negativePasswordArr = [
 
 export const NEGATIVE_FIELD_DATA =
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem';
+
+export const ERROR_COLOR = 'rgb(255, 81, 81)';
---
+++ tests_advance/authorization.spec.js
@@ -0,0 +1,26 @@
+import { expect } from '@playwright/test';
+import { test } from '../fixtures/base';
+import { ERROR_COLOR } from '../testData';
+import { description, tag, severity, Severity, epic, step } from 'allure-js-commons';
+
+test.describe('Negative tests for Authorization process', () => {
+    test('SP-13/SP-7/01 | Verify non-successful authorization of Business user in case of empty "Email Address" field', async ({ page, loginPage, signPage }) => {
+        await description('To verify non-successful authorization of Business user in case of empty "Email Address" field.');
+        await severity(Severity.NORMAL);
+        await epic('Authorization');
+        await tag('Login: negative');
+
+        await step('Navigate to Login page.', async () => {
+            await page.goto('/');
+        });
+        await loginPage.fillEmailAddressInput('');
+        await loginPage.fillPasswordInput(process.env.USER_PASSWORD);
+
+        await step('Verify the email field turns red, indicating an error.', async () => {
+            await expect(loginPage.emailAddressInput).toHaveCSS('color', ERROR_COLOR);
+        });
+        await step('Verify the error message "Required"', async () => {
+            await expect(loginPage.emailAddressError).toHaveText('Required');
+        });
+    });
+});
