ae279acf39ecbf2df5d3d85ff1b6641336ad45ca
MlimaU <129126676+mlimau@users.noreply.github.com>
2024-07-26 16:17:10 -0400

    Formatted

--- tests_advance/template.spec.js
+++ tests_advance/template.spec.js
@@ -1,25 +1,14 @@
 import { expect } from '@playwright/test';
 import { test } from '../fixtures/base.js';
-import {
-    CREATE_TEMPLATE,
-    TEMPLATES_STATUS,
-    UPLOAD_FILE_PATH,
-    JIRA_LINK,
-    TOAST_MESSAGE
-
-    } from '../testData.js';
-
-import { createTemplate } from '../helpers/preconditions.js';
-import { description, tags, severity, link, epic, step } from 'allure-js-commons';
+import { CREATE_TEMPLATE, TEMPLATES_STATUS, UPLOAD_FILE_PATH, JIRA_LINK, TOAST_MESSAGE } from '../testData.js';
+import { description, tags, severity, Severity, link, epic, step } from 'allure-js-commons';
 
 test.describe('Negative tests for Templates Options', () => {
     test('SP27/SP37/1 | Verify user can not create the template missing Role Field.', async ({
         createBusinessUserAndLogin,
         signPage,
-        prepareForSignatureModal,
         templatesPage,
         createNewTemplatePage,
-        successModal,
     }) => {
         test.setTimeout(440 * 1000);
         await description(
@@ -29,12 +18,12 @@ test.describe('Negative tests for Templates Options', () => {
         await epic('Templates');
         await tags('Template');
         await link(`${JIRA_LINK}SP-37`);
-   
+
         await signPage.sideMenu.clickTemplates();
         await templatesPage.sideMenuTemplates.clickCreateTemplate();
         await createNewTemplatePage.fillTemplateNameField(CREATE_TEMPLATE.nameField);
         await createNewTemplatePage.fillOptionalMessageField(CREATE_TEMPLATE.optionalMessage);
-        
+
         await createNewTemplatePage.fileUploader.uploadFile(UPLOAD_FILE_PATH.csvDocument);
         await createNewTemplatePage.clickFillTemplateBtn();
 
@@ -43,9 +32,9 @@ test.describe('Negative tests for Templates Options', () => {
         });
 
         await signPage.sideMenu.clickTemplates();
-        
+
         await step('Verify created template has "Draft" status.', async () => {
             await expect(await templatesPage.table.documentStatus).toHaveText(TEMPLATES_STATUS.draft);
         });
     });
-})
+});
