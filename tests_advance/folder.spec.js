b43042e30656c579f2a110a0649636b4e48397c3
Elgrueber <89706559+Elgrueber@users.noreply.github.com>
2024-08-06 13:15:28 -0400

    SP22/SP48/1 Verify FREE user can move folder to folder (#35)
    * add test about move folder to folder + format
    
    * add one more verify

--- tests_advance/folder.spec.js
+++ tests_advance/folder.spec.js
@@ -2,7 +2,7 @@ import { expect } from '@playwright/test';
 import { test } from '../fixtures/base';
 import { createFolder } from '../helpers/preconditions.js';
 import { signInRequest, createFolderRequest } from '../helpers/apiCalls';
-import { FOLDER_NAME, JIRA_LINK, TOAST_MESSAGE, FILL_RENAME_FOLDER_NAME } from '../testData';
+import { FOLDER_NAME, JIRA_LINK, TOAST_MESSAGE, FILL_RENAME_FOLDER_NAME, FOLDER_NAME_SECOND } from '../testData';
 import { description, tag, tags, severity, Severity, epic, step, link, feature } from 'allure-js-commons';
 
 test.describe('Folder API', () => {
@@ -99,10 +99,10 @@ test.describe('Folders in case of FREE User', () => {
         test.setTimeout(150 * 1000);
         await description('To verify Free user is able to rename folder.');
         await severity(Severity.NORMAL);
-        await tag('Rename Folder', 'Free User');
+        await tag('Folder to folder', 'Free User');
         await epic('Folders');
         await feature('Free user');
-        await link(`${JIRA_LINK}SP-46`, 'Jira task link');
+        await link(`${JIRA_LINK}SP-48`, 'Jira task link');
 
         await createFolder(signPage, documentsPage, createFolderModal, FOLDER_NAME);
         await signPage.sideMenu.clickDocuments();
@@ -113,6 +113,40 @@ test.describe('Folders in case of FREE User', () => {
 
         await step('Verify new value has been saved.', async () => {
             expect(await documentsPage.table.getTitleFolder()).toBe(FILL_RENAME_FOLDER_NAME);
+        });
+    });
+
+    test('SP22/SP46/1 | Verify FREE user is able to move folder to foler.', async ({
+        createFreeUserAndLogin,
+        signPage,
+        documentsPage,
+        createFolderModal,
+        moveToFolderModal,
+    }) => {
+        test.setTimeout(150 * 1000);
+        await description('To verify Free user is able to move folder to foler.');
+        await severity(Severity.NORMAL);
+        await tag('Rename Folder', 'Free User');
+        await epic('Folders');
+        await feature('Free user');
+        await link(`${JIRA_LINK}SP-46`, 'Jira task link');
+
+        await createFolder(signPage, documentsPage, createFolderModal, FOLDER_NAME);
+        await createFolder(signPage, documentsPage, createFolderModal, FOLDER_NAME_SECOND);
+        await signPage.sideMenu.clickDocuments();
+        await documentsPage.table.clickFirstOptionsBtn();
+        await documentsPage.table.clickMoveToBtn();
+        await moveToFolderModal.selectFolder(FOLDER_NAME);
+        await moveToFolderModal.clickMoveToFolderBtn();
+
+        await step('Verify toast notification about successful folder to folder move is displayed.', async () => {
+            await documentsPage.toast.toastBody.waitFor({ state: 'visible' });
+            await expect(documentsPage.toast.toastBody).toHaveText(TOAST_MESSAGE.folderMovedToFolder);
+        });
+
+        await step('Verify the chosen folder inside another folder.', async () => {
+            await documentsPage.table.openFolder(FOLDER_NAME);
+            expect(await documentsPage.table.getTitleFolder()).toEqual(FOLDER_NAME_SECOND);
         });
     });
 });
